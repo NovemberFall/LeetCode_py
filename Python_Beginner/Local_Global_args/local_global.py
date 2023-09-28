806360a1a977041d9ac0883349f8f6831d9fc683
TomZheng <1219.zheng@gmail.com>
2023-09-28 16:11:57 -0700

    leetcode by python

---
+++ Python_Beginner/Local_Global_args/local_global.py
@@ -0,0 +1,12 @@
+order = "Groceries"
+service = "Instacart"
+
+
+def print_details_of_order():
+    order = "Meat"
+    service = "DoorDash"
+
+    print("Order:", order)
+    print("Service:", service)
+
+print_details_of_order()

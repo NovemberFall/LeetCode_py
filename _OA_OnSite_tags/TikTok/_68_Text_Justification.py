6f33101098852cd0d08f7d5942f5815df4f50729
TomZheng <1219.zheng@gmail.com>
2025-03-04 16:33:39 -0800

    lc 68

--- _OA_OnSite_tags/TikTok/_68_Text_Justification.py
+++ _OA_OnSite_tags/TikTok/_68_Text_Justification.py
@@ -4,42 +4,42 @@ from typing import List
 class Solution:
     def fullJustify(self, words: List[str], maxWidth: int) -> List[str]:
         res = []
-        curr_line = []
-        curr_len = 0
+        line_words = []
+        line_length = 0
 
         for word in words:
             # Check if adding this word would exceed the maxWidth
-            if curr_len + len(word) + len(curr_line) > maxWidth:
-                # If yes, we need to justify the current line
-                if len(curr_line) == 1:
-                    # Special case: only one word in the line
-                    res.append(curr_line[0] + ' ' * (maxWidth - len(curr_line[0])))
+            if line_length + len(word) + (len(line_words) - 1) >= maxWidth:
+                # If the current line is full, justify the text
+                if len(line_words) == 1:
+                    # Special case: only one word in the line (left-aligned)
+                    res.append(line_words[0] + ' ' * (maxWidth - len(line_words[0])))
                 else:
                     # Distribute spaces evenly
-                    total_spaces = maxWidth - curr_len
-                    space_between_words = total_spaces // (len(curr_line) - 1)
-                    extra_spaces = total_spaces % (len(curr_line) - 1)
+                    total_spaces = maxWidth - line_length
+                    space_between_words = total_spaces // (len(line_words) - 1)
+                    extra_spaces = total_spaces % (len(line_words) - 1)
 
-                    line = curr_line[0]
-                    for i in range(1, len(curr_line)):
+                    justified_line = line_words[0]
+                    for i in range(1, len(line_words)):
                         if i <= extra_spaces:
                             spaces = ' ' * (space_between_words + 1)
                         else:
                             spaces = ' ' * (space_between_words)
-                        line += spaces + curr_line[i]
+                        justified_line += spaces + line_words[i]
 
-                    res.append(line)
+                    res.append(justified_line)
 
                 # Reset for the next line
-                curr_line = [word]
-                curr_len = len(word)
+                line_words = [word]
+                line_length = len(word)
             else:
                 # Add word to the current line
-                curr_line.append(word)
-                curr_len += len(word)
+                line_words.append(word)
+                line_length += len(word)
 
         # Handle the last line (left-justified)
-        last_line = ' '.join(curr_line)
+        last_line = ' '.join(line_words)
         res.append(last_line + ' ' * (maxWidth - len(last_line)))
 
         return res

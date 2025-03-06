78b5ddf2db7584e14e181669332a637ad88f251a
NovemberFall <1219.zheng@gmail.com>
2025-03-05 21:22:47 -0800

    lc 309

--- Dynamic_Programming/_309_BestTime_to_Buy_and_Sell_Stock_with_Cooldown/dfs.py
+++ Dynamic_Programming/_309_BestTime_to_Buy_and_Sell_Stock_with_Cooldown/dfs.py
@@ -5,11 +5,14 @@ class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         return self.dfs(prices, True, 0)
 
-    def dfs(self, prices: List[int], is_buy, index):
-        if index == len(prices):
+    def dfs(self, prices, is_buy, index):
+        if index >= len(prices):
             return 0
 
         if is_buy:
-            return max(self.dfs(prices, False, index + 1) - prices[index],
-                       0 + self.dfs(prices, True, index + 1))
-
+            buy = - prices[index] + self.dfs(prices, False, index + 1)
+            skip = 0 + self.dfs(prices, True, index + 1)
+            return max(buy, skip)
+        else:
+            sell = prices[index] + self.dfs(prices, True, index + 2)
+                       0 + self.dfs(prices, False, index + 1))

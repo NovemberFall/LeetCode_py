5ee8356001665da7e6b5a0515019ae82ed5f357a
NovemberFall <1219.zheng@gmail.com>
2025-03-07 00:32:03 -0800

    lc 121

---
+++ Dynamic_Programming/_121_BestTime_to_Buy_and_Sell_Stock_I/tabulation.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        n = len(prices)
+        dp = [[[0 for _ in range(2)] for _ in range(2)] for _ in range(n + 1)]
+        for index in range(n - 1, -1, -1):
+            for buy in range(2):
+                for t_l in range(1, 2):
+                    if buy:
+                        dp[index][buy][t_l] = max(-prices[index] + dp[index + 1][0][t_l], dp[index + 1][1][t_l])
+                    else:
+                        dp[index][buy][t_l] = max(prices[index] + dp[index + 1][1][t_l - 1], dp[index + 1][0][t_l])
+
+        return dp[0][1][1]

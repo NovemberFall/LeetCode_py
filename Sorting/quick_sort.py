74812d2eb52a13de67dadaa7f42adbfe40c937cb
TomZheng <1219.zheng@gmail.com>
2025-03-16 17:00:59 -0700

    update

--- Sorting/quick_sort.py
+++ Sorting/quick_sort.py
@@ -4,4 +4,31 @@ class Solution(object):
         input: int[] array
         return: int[]
         """
-        
+        self.quick_sort(array, 0, len(array) - 1)
+        return array
+
+    def quick_sort(self, array, left, right):
+        if left >= right:
+            return
+        pivot = self.partition(array, left, right)
+        self.quick_sort(array, left, pivot - 1)
+        self.quick_sort(array, pivot + 1, right)
+
+    def partition(self, array, left, right):
+        leftBound = left
+        rightBound = right - 1
+        import random
+        rand = random.randint(leftBound, rightBound)
+        array[rand], array[right] = array[right], array[rand]
+        pivot = array[right]
+        while leftBound <= rightBound:
+            if array[leftBound] < pivot:
+                leftBound += 1
+            elif array[rightBound] >= pivot:
+                rightBound -= 1
+            elif array[leftBound] >= pivot and array[rightBound] < pivot:
+                array[leftBound], array[rightBound] = array[rightBound], array[leftBound]
+                leftBound += 1
+                rightBound -= 1
+        array[leftBound], array[right] = array[right], array[leftBound]
+        return leftBound

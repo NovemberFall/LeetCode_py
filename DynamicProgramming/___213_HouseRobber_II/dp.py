014f610176bebec07a56735f32cb8b4fcbfe9699
NovemberFall <1219.zheng@gmail.com>
2025-05-16 21:27:10 -0700

    lc 213

--- DynamicProgramming/___213_HouseRobber_II/dp.py
+++ DynamicProgramming/___213_HouseRobber_II/dp.py
@@ -2,3 +2,19 @@ from typing import List
 
 class Solution:
     def rob(self, nums: List[int]) -> int:
+        n = len(nums)
+        if n < 2:
+            return nums[0]
+        if n == 2:
+            return max(nums[0], nums[1])
+
+        return max(self.f(nums[:n - 1]), self.f(nums[1:]))
+
+    def f(self, nums: List[int]) -> int:
+        n = len(nums)
+        dp = [0] * n
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
+        for i in range(2, n):
+            dp[i] = max(dp[i - 1], dp[i - 2] + nums[i])
+        return dp[n - 1]

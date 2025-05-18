4c74d76650ccd55e2f56c8a77681f4b72fcf7efb
NovemberFall <1219.zheng@gmail.com>
2025-05-17 21:12:48 -0700

    update

--- DynamicProgramming/memo_377_CombinationSum_IV/dp_2d_.py
+++ DynamicProgramming/memo_377_CombinationSum_IV/dp_2d_.py
@@ -3,3 +3,7 @@ from typing import List
 
 class Solution:
     def combinationSum4(self, nums: List[int], target: int) -> int:
+        # since for num[i] from value 1 to target, the length is target
+        length = target
+        dp = [[0] * (length + 1) for _ in range(target + 1)]
+        dp[0][0] = 1
